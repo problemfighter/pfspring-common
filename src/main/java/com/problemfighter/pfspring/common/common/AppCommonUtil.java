75e47f265d6eb9703ab461efb9f78722348f695b
hmtmcse <hmtm.cse@gmail.com>
2021-01-26 12:53:11 +0600

    Added Organization feature

--- src/main/java/com/problemfighter/pfspring/common/common/AppCommonUtil.java
+++ src/main/java/com/problemfighter/pfspring/common/common/AppCommonUtil.java
@@ -1,5 +1,6 @@
 package com.problemfighter.pfspring.common.common;
 
+import java.util.Random;
 import java.util.UUID;
 
 public final class AppCommonUtil {
@@ -8,5 +9,22 @@ public final class AppCommonUtil {
         return UUID.randomUUID().toString().toUpperCase();
     }
 
+
+    public static String randomAlphanumeric(Integer limit) {
+        int leftLimit = 48;
+        int rightLimit = 122;
+        Random random = new Random();
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(limit)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+
+    public static String randomNumeric(Integer limit, Integer boundary) {
+        Random rand = new Random();
+        return String.format("%0" + limit + "d", rand.nextInt(boundary));
+    }
 
 }
